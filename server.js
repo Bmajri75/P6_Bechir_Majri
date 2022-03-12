57075d64afbd3a70d491772e1c7958e68f840f0c
Bechir Majris <bmajri@gmail.com>
2022-03-12 21:11:54 +0100

    first push

---
+++ server.js
@@ -0,0 +1,8 @@
+// je requier express
+const express = require('express');
+
+// je place dans app le module express
+const app = express();
+
+// 300 est le port sur le quel va tourner mon server;
+const port = 3000;
