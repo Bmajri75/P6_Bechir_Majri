1bc5e35537ade106745143b8ffd826fbf26610d4
Bechir Majris <bmajri@gmail.com>
2022-03-18 23:16:56 +0100

    instalation mongoos et Express

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-// je requier express & mongoose
+// ======= Epress =======
+// je requier express
 const express = require('express');
-const mongoose = require('mongoose');
 // je place dans app le module express
 const app = express();
+// 3000 est le port sur le quel va tourner mon server;
+const port = 3000;
+
+//===== GESTION DataBase ====
+// requiere Mongoose
+const mongoose = require('mongoose');
 
 // conexion a mongoDb
-mongoose.connect('mongodb+readWriteAnyDatabase@admin://jimbob:Techwin75020@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://bmajri:Techwin75020@cluster0.6jkmr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch(() => console.error('Connexion à MongoDB échouée !'));
 
-// 3000 est le port sur le quel va tourner mon server;
-const port = 3000;
-
+// ===== mes Routes =====
 app.get('/', (req, res) => {
   res.send('test')
 })
-
+// ===== lancement du server ======
 app.listen(port, () => {
   console.log(`Server ouvert sur le PORT : ${port}`)
 })
+
 
 module.exports = app;
