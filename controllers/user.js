38bb930bc73e4498677021a8fce2538357994112
Bechir Majris <bmajri@gmail.com>
2022-03-23 23:11:42 +0100

    mise en place du Hashage du Mot de Passe

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,27 @@
 // c'est Ici que vas etre executer toutes les commandes pour ma routes 
+const bcrypt = require('bcrypt'); // bcrypt pour hashage des mots de passe
 
 // je recupere les model cree 
 const userModel = require('../models/Users');
 
 exports.inscription = (req, res, next) => {
-
   delete req.body._id;// on supprime le champs Id qui sera cree automatiquement avent de cree mon instance
 
-  // je cree une instance du model cree c'est a dir Users qui est dans la constante userModel
-  const user = new userModel({
-    ...req.body // l'operateur vas recuperer tout ce qui peut etre envoyer dans les champs dans le body et detail seul les titre email etc..
-  });
-  user.save() // methode qui envoie a la dataBase l'instance de userModel
-    .then(() => res.status(201).json({
-      message: 'Profil Cree !'
-    })) // je capture la response 
-    .catch(error => res.status(400).json(`il y'a une erreur ${error}`)); // je capture l'erreur et je l'affiche en Json
+  // hachage du Mdp 
+  bcrypt.hash(req.body.password, 10)// hashage 10
+    .then((hash) => {
 
-  console.log(user);
+      // je cree une instance du model cree c'est a dir Users qui est dans la constante userModel
+      const user = new userModel({
+        ...req.body,// l'operateur vas recuperer tout ce qui peut etre envoyer dans les champs dans le body et detail seul les titre email etc..
+        password: hash
+      });
+      user.save() // methode qui envoie a la dataBase l'instance de userModel
+        .then(() => res.status(201).json({
+          message: 'Profil Crèe !'
+        })) // la response si tout vas bien
+        .catch(error => res.status(400).json({ error } // la reponse si tout vas mal
+          .send(console.log(` il y'a une erreur ${error}`))));// avec un msg dans la console
+    })
 
-}
+};
