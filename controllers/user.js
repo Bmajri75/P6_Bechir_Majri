906f3824632ef96057af536a3e2d127e6d0126b1
Bechir Majris <bmajri@gmail.com>
2022-03-23 23:39:04 +0100

    mise en place du Hashage du Mot de Passe

--- controllers/user.js
+++ controllers/user.js
@@ -10,18 +10,21 @@ exports.inscription = (req, res, next) => {
   // hachage du Mdp 
   bcrypt.hash(req.body.password, 10)// hashage 10
     .then((hash) => {
-
       // je cree une instance du model cree c'est a dir Users qui est dans la constante userModel
       const user = new userModel({
-        ...req.body,// l'operateur vas recuperer tout ce qui peut etre envoyer dans les champs dans le body et detail seul les titre email etc..
+        email: req.body.email,// l'operateur vas recuperer tout ce qui peut etre envoyer dans les champs dans le body et detail seul les titre email etc..
         password: hash
       });
       user.save() // methode qui envoie a la dataBase l'instance de userModel
         .then(() => res.status(201).json({
           message: 'Profil Crèe !'
         })) // la response si tout vas bien
-        .catch(error => res.status(400).json({ error } // la reponse si tout vas mal
+        .catch(err => res.status(400).json({ err } // la reponse si tout vas mal
           .send(console.log(` il y'a une erreur ${error}`))));// avec un msg dans la console
+    })
+    .catch((err) => {
+      res.status(500).json({ err }) // capture err 500 en cas d'erreur serveur
+        .send(console.log(` erreur : ${err}`))
     })
 
 };
